b8156b898aeb6e623db9203a6875a0a3a352131f
Orient55 <122955312+Orient55@users.noreply.github.com>
2023-03-30 17:02:04 +0200

    add function to change color when hovering

--- script.js
+++ script.js
@@ -10,10 +10,20 @@ for (let i = 1; i < 17; i++){
     for (let j = 0; j < 16; j++){
         let column = document.createElement('div');
         column.className = 'column';
-        //column.classList.add = 'rows';
         row.appendChild(column);
         column.textContent = `${j}`;
 
     }
+}
+
+
+
+let hover = document.getElementsByClassName('column');
+for (let i = 0; i < hover.length; i++){
+    hover[i].addEventListener('mouseover', function (){
+        hover[i].classList.add('hover');
+        //alert('test');
+
+    })
 }
 
